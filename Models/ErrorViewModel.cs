6ad398a9c1e9d614465ad44d2bce442d2ab007e8
Trịnh Minh <trinhquangminh1109@gmail.com>
2022-10-06 08:00:40 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TrinhQuangMinhBTH2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
